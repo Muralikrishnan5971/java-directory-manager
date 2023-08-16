a2b7d75044013dae887433bacef4af8242d53799
Murali Krishnan <muralikrishnan.pro@gmail.com>
2023-08-16 14:05:47 +0530

    list all files recursively

--- src/main/java/com/dm/Manager.java
+++ src/main/java/com/dm/Manager.java
@@ -1,10 +1,29 @@
 package com.dm;
 
+import java.io.File;
+
 public class Manager {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        String source = "/Users/muralikrishnan/Downloads";
+
+        File file = new File(source);
+
+        File[] fileList = file.listFiles();
+
+        for (int i = 0; i < fileList.length; i++) {
+
+            if (fileList[i].isDirectory()) {
+
+                File[] nestedFileList = fileList[i].listFiles();
+
+                for (int j = 0; j < nestedFileList.length; j++) {
+                    System.out.println(nestedFileList[j].getName());
+                }
+
+            }
+        }
 
     }
 }
